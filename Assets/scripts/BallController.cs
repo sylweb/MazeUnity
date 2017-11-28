9c3c1501aed94bc8aed94934be0d2c5556966183
sylvain PEQUIGNOT <sylvain.pequignot@vetoquinol.com>
2017-11-28 10:51:22 +0100

    <commit message>

---
+++ Assets/scripts/BallController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public float speed;
+	public float jump_max_force;
+
+	private Rigidbody body;
+	private bool canJump=true;
+
+
+	void Start ()
+	{
+		body = GetComponent<Rigidbody>();
+
+		canJump = true;
+	}
+
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space) && canJump == true) {
+			body.AddForce (0.0f, jump_max_force, 0.0f);
+			canJump = false;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		if (SystemInfo.deviceType == DeviceType.Desktop) 
+		{ 
+			// Player movement in desktop devices
+			// Definition of force vector X and Y components
+			float moveHorizontal = Input.GetAxis("Horizontal");
+			float moveVertical = Input.GetAxis("Vertical");
+			// Building of force vector
+			Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+			// Adding force to rigidbody
+			body.AddForce(movement * speed * Time.deltaTime);
+		}
+		else
+		{
+			// Player movement in mobile devices
+			// Building of force vector 
+			Vector3 movement = new Vector3 (Input.acceleration.x, 0.0f, Input.acceleration.y);
+			// Adding force to rigidbody
+			body.AddForce(movement * speed * Time.deltaTime);
+		}
+
+		//reset ball position when player falls
+		if (transform.position.y < -10) {
+			Vector3 orig = new Vector3 (0.0f, 0.5f, 0.0f);
+			transform.position = orig;
+			body = GetComponent<Rigidbody>();
+		}
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.tag == "floor_piece")
+		{
+			canJump = true;
+		}
+	}
+}
---
+++ Assets/scripts/MainCameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraController : MonoBehaviour {
+
+	public GameObject player;       //Public variable to store a reference to the player game object
+
+
+	private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+
+	private float offsetX;
+	private float offsetZ;
+
+	// Use this for initialization
+	void Start () 
+	{
+		offsetX = transform.position.x- player.transform.position.x;
+		offsetZ = transform.position.z - player.transform.position.z;
+	}
+
+	// LateUpdate is called after Update each frame
+	void LateUpdate () 
+	{
+		//The camera will follow x and z movement of the player but not y because we don't want the camera to follow the player when he jumps or when he falls
+		Vector3 pos = new Vector3(player.transform.position.x + offsetX, transform.position.y, player.transform.position.z + offsetZ);
+		transform.position = pos;
+	}
+}
