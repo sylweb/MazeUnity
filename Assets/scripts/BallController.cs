28acafc4a326eba5f71b369a0d4bfdee20243231
sylvain PEQUIGNOT <sylvain.pequignot@vetoquinol.com>
2017-12-06 16:25:50 +0100

    Some work done

--- Assets/scripts/BallController.cs
+++ Assets/scripts/BallController.cs
@@ -4,123 +4,85 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
 
-
-	public float jump_max_force;
-
+	public Transform target;
 	private Rigidbody body;
-	private float speed;
-	private bool canJump=true;
+	public float speed;
+	public float smoothing = 1.0f; 
+	private Vector3 offset;
 
-	void Start ()
-	{
-		Screen.autorotateToLandscapeLeft = true;
-		Screen.autorotateToLandscapeRight = false;
-		Screen.autorotateToPortrait = false;
-		Screen.autorotateToPortraitUpsideDown = false;
-		body = GetComponent<Rigidbody>();
-		canJump = true;
+	private Ray shootRay;
+	public LineRenderer gunLine;
+
+	GameObject[] floor;
+	GameObject coin;
+
+	// Use this for initialization
+	void Start () {
+		this.body = GetComponent<Rigidbody> ();
+		offset = target.position - transform.position;
+		floor = GameObject.FindGameObjectsWithTag("floor_piece");
+		coin = GameObject.FindGameObjectWithTag ("collectable");
+		gunLine = GetComponent <LineRenderer> ();
+		gunLine.enabled = true;
+		gunLine.SetPosition (0, transform.position);
+	}
+
+	void ResetGame() {
+		for (int i = 0; i < floor.Length; i++) {
+			floor [i].GetComponent<Renderer> ().enabled = true;
+			floor [i].GetComponent<Collider> ().enabled = true;
+		}
+
+		transform.position = new Vector3 (-0.75f,-0.5f,0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (transform.position.y < -2)
+			ResetGame ();
 
 		if (SystemInfo.deviceType == DeviceType.Desktop) {
-			speed = 200.0f;
-		} else
-			speed = 300.0f;
 
-	}
+			float xAxis = 0.0f;
+			float yAxis = 0.0f;
 
+			Vector3 vec = new Vector3 (Time.deltaTime * speed * Input.GetAxis ("Horizontal"), 0.0f, Time.deltaTime * speed * Input.GetAxis ("Vertical"));
+			this.body.AddForce (vec);
+		} else {
+			Vector3 dir = Vector3.zero;
+			dir.x = -Input.acceleration.y;
+			dir.z = Input.acceleration.x;
+			if (dir.sqrMagnitude > 1) dir.Normalize ();
 
-	void Update() {
-		
-		if (SystemInfo.deviceType == DeviceType.Desktop) 
-		{ 
-			// Player movement in desktop devices
-			// Definition of force vector X and Y components
-			float moveHorizontal = Input.GetAxis("Horizontal");
-			float moveVertical = Input.GetAxis("Vertical");
-			// Building of force vector
-			Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
-			// Adding force to rigidbody
-			body.AddForce(movement * speed * Time.deltaTime);
-
-			if (Input.GetKeyDown(KeyCode.Space) && canJump == true) {
-				body.AddForce (0.0f, jump_max_force, 0.0f);
-				canJump = false;
-			}
+			dir = dir * Time.deltaTime * speed;
+			this.body.AddForce (dir);
 		}
-		else
-		{
-			// Player movement in mobile devices
-			// Building of force vector 
-			Vector3 movement = new Vector3 (Input.acceleration.x, 0.0f, Input.acceleration.y);
-			// Adding force to rigidbody
-			body.AddForce(movement * speed * Time.deltaTime);
-		}
+			
+		// Smoothly interpolate between the camera's current position and it's target position.
+		Vector3 targetCamPos = transform.position + offset;//
+		target.position = Vector3.Lerp (target.position, targetCamPos, smoothing);
 
-		//reset ball position when player falls
-		if (transform.position.y < -5) {
-			resetPosition ();
-		}
-	}
-		
-	void OnCollisionEnter (Collision other)
-	{
-		//If it's floor
-		if(other.gameObject.tag == "floor_piece")
-		{
-			canJump = true;
-		} 
+		gunLine.SetPosition (0, transform.position);
+		shootRay.origin = transform.position;
+		shootRay.direction = coin.transform.position;
+		gunLine.SetPosition (1, coin.transform.position);
+
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.gameObject.tag == "collectable") {
-
-			//Generate a new random position for the callectable
-			float posX = Random.Range(-9.0f,7.0f);
-			float posZ = Random.Range(-9.0f,7.0f);
-			other.gameObject.GetComponent<Transform> ().position = new Vector3 (posX, -0.5f, posZ);
-
-			//Remove randomly 3 floor pieces
-			randomlyRemoveFloorPiece ();
-		}
+		if (other.gameObject == coin) HitACoin ();
 	}
 
-	void resetPosition() {
-		body.velocity = Vector3.zero;
-		body.angularVelocity = Vector3.zero;
-		body.inertiaTensorRotation = Quaternion.identity;
-		body.inertiaTensor = Vector3.zero;
-		body.isKinematic = true;
-		body.isKinematic = false;
-		Vector3 orig = new Vector3 (0.0f, 0.5f, 0.0f);
-		transform.position = orig;
-	}
+	void HitACoin() {
+		//Randomly generate a new position for the orb
+		int x = (int) Random.Range (-9.0f, 7.0f);
+		int z = (int) Random.Range (-9.0f, 7.0f);
+		coin.transform.position = new Vector3(x,-0.5f,z);
 
-	void randomlyRemoveFloorPiece() {
-		
-		//Make one floor piece disapear
-		bool foundOneToRemove = false;
-		do {
-			float posX = Random.Range (-9.0f, 7.0f);
-			float posZ = Random.Range (-9.0f, 7.0f);
-
-			//We don't want to make the floor under the ball disapear
-			if (posX < transform.position.x && transform.position.x < posX + 1)
-				break;
-			if (posZ < transform.position.z && transform.position.z < posZ + 1)
-				break;
-
-			foreach (GameObject floorPiece in GameObject.FindGameObjectsWithTag ("floor_piece")) {
-				if (floorPiece.GetComponent<Transform> ().transform.position.x > posX && floorPiece.GetComponent<Transform> ().transform.position.x < posX + 1) {
-					if (floorPiece.GetComponent<Transform> ().transform.position.z > posZ && floorPiece.GetComponent<Transform> ().transform.position.z < posZ + 1) {	
-
-						if (floorPiece.GetComponent<Renderer> ().enabled == true) {
-							floorPiece.GetComponent<Renderer> ().enabled = false;
-							floorPiece.GetComponent<Collider> ().enabled = false;
-							foundOneToRemove = true;
-						}
-						break;
-					}
-				}
-			}
-		} while(!foundOneToRemove);
+		//Randomly delete a floor piece
+		int floorIndex = (int)Random.Range (0.0f, floor.Length-1);
+		floor[floorIndex].GetComponent<MeshRenderer>().enabled = false;
+		floor [floorIndex].GetComponent<Collider> ().enabled = false;
 	}
 }
