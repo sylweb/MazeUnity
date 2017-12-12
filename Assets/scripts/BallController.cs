03ddd172e77392a5a34dcfc71ea6f20703b82367
sylvain PEQUIGNOT <sylvain.pequignot@vetoquinol.com>
2017-12-12 09:30:14 +0100

    improving settings for android plateform. Fixing some bugs.

--- Assets/scripts/BallController.cs
+++ Assets/scripts/BallController.cs
@@ -5,22 +5,33 @@ using UnityEngine;
 public class BallController : MonoBehaviour {
 
 	public Transform camPos;
+	public GameObject scoreText;
+	public GameObject fpsText;
 	public float smoothing;
 
 	private Rigidbody body; 
 	private LineRenderer directionToNextCoin;
-	private Vector3 offset;
+	private Vector3 camOffset;
+	private Vector3 scoreOffset;
+	private Vector3 fpsOffset;
 
 	private GameObject[,] floor;
 	private GameObject coin;
+	private float coinPositionY;
 	private float speed;
+	private int score = 0;
+	private Vector3 ballOrigine;
+
 
 	// Use this for initialization
 	void Start () {
 		Application.targetFrameRate = 60;
 		Screen.sleepTimeout = SleepTimeout.NeverSleep;
 		this.body = GetComponent<Rigidbody> ();
-		offset = camPos.position - transform.position;
+		camOffset = camPos.position - transform.position;
+		scoreOffset = scoreText.transform.position - transform.position;
+		fpsOffset = fpsText.transform.position - transform.position;
+
 
 		GameObject[] temp = GameObject.FindGameObjectsWithTag("floor_piece");
 
@@ -30,12 +41,18 @@ public class BallController : MonoBehaviour {
 		}
 
 		coin = GameObject.FindGameObjectWithTag ("collectable");
+		this.coinPositionY = coin.transform.position.y;
 		directionToNextCoin = GetComponent <LineRenderer> ();
 		directionToNextCoin.enabled = true;
 		directionToNextCoin.startColor = Color.green;
 		directionToNextCoin.endColor = Color.green;
 		speed = 600.0f;
 		if (SystemInfo.deviceType == DeviceType.Handheld) speed = (speed * 70 / 100) + speed;
+
+		scoreText.GetComponent<TextMesh> ().text = "Score : 0";
+		score = 0;
+
+		ballOrigine = this.transform.position;
 	}
 
 	void ResetGame() {
@@ -44,7 +61,7 @@ public class BallController : MonoBehaviour {
 			floor [i].GetComponent<Collider> ().enabled = true;
 		}*/
 
-		transform.position = new Vector3 (-1.0f,-0.5f,-2.0f);
+		transform.position = ballOrigine;
 	}
 	
 	// Update is called once per frame
@@ -52,24 +69,16 @@ public class BallController : MonoBehaviour {
 
 		if (Input.GetKeyDown(KeyCode.Escape)) 
 			Application.Quit(); 
-
-		if (SystemInfo.deviceType == DeviceType.Desktop) {
-
-			Vector3 vec = new Vector3 (Time.deltaTime * speed * Input.GetAxis ("Horizontal"), 0.0f, Time.deltaTime * speed * Input.GetAxis ("Vertical"));
-			this.body.AddForce (vec);
-		} else {
-			Vector3 dir = Vector3.zero;
-			dir.x = Input.acceleration.x;
-			dir.z = Input.acceleration.y;
-			if (dir.sqrMagnitude > 1) dir.Normalize ();
-
-			dir = dir * Time.deltaTime * speed;
-			this.body.AddForce (dir);
-		}
 			
 		// Smoothly interpolate between the camera's current position and it's target position.
-		Vector3 targetCamPos = transform.position + offset;//
+		Vector3 targetCamPos = transform.position + camOffset;//
 		camPos.position = Vector3.Lerp (camPos.position, targetCamPos, smoothing);
+
+		//Replace HUD text
+		scoreText.transform.position = transform.position + scoreOffset;
+		fpsText.transform.position = transform.position + fpsOffset;
+		int fps = (int)(1.0f / Time.deltaTime);
+		fpsText.GetComponent<TextMesh> ().text = ""+fps;
 
 		//A ray that point to the next coin location
 		directionToNextCoin.SetPosition (0, transform.position);
@@ -102,6 +111,9 @@ public class BallController : MonoBehaviour {
 	}
 
 	void HitACoin() {
+
+		score = score + 1;
+		scoreText.GetComponent<TextMesh> ().text = "Score : "+score;
 		
 		//Randomly delete a floor piece
 		int floorXIndex = 0;
@@ -116,10 +128,17 @@ public class BallController : MonoBehaviour {
 		//Randomly generate a new position for the orb but not in a hole
 		int x = 0;
 		int z = 0;
+		bool tooCloseToBall = false;
 		do {
+
 			x = (int)Random.Range (-9.0f, 7.0f);
 			z = (int)Random.Range (-9.0f, 7.0f);
-		} while(floor [x+9, z+9] == null || floor [x+9, z+9].GetComponent<Collider> ().enabled == false);
-		coin.transform.position = new Vector3(x,-0.5f,z);
+
+			tooCloseToBall = false;
+			if(transform.position.x > (x-1) && transform.position.x < (x+1) && transform.position.z > (z-1) && transform.position.z < (z+1))
+				tooCloseToBall = true;
+
+		} while(floor [x+9, z+9] == null || floor [x+9, z+9].GetComponent<Collider> ().enabled == false || tooCloseToBall);
+		coin.transform.position = new Vector3(x,coinPositionY,z);
 	}
 }
